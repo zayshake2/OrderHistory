443b6882b8205d2c8b798b5f15838faccc86704c
Zaynab Shaikh <zayshake2@gmail.com>
2022-04-27 00:45:31 -0500

    Updated vs

--- app/src/main/java/com/example/orderhistory/MainActivity.java
+++ app/src/main/java/com/example/orderhistory/MainActivity.java
@@ -31,9 +31,9 @@ public class MainActivity extends AppCompatActivity {
         ListView Order_History = findViewById(R.id.Order_History);
 
         List<String> orders = new ArrayList<>();
-        orders.add("Order 1");
-        orders.add("Order 2");
-        orders.add("Order 3");
+        orders.add("Order 1003439256");
+        orders.add("Order 1007872374");
+        orders.add("Order 1002838748");
 
 
 
@@ -47,11 +47,11 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 if(i==0) {
 
-                    startActivity(new Intent(MainActivity.this, Order1Activity.class));
+                    startActivity(new Intent(MainActivity.this, Order3Activity.class));
 
                 }
                 else if(i==1) {
-                    startActivity(new Intent(MainActivity.this, Order2Activity.class));
+                    startActivity(new Intent(MainActivity.this, Order3Activity.class));
                 }
                 else if(i==2){
                     startActivity(new Intent(MainActivity.this, Order3Activity.class));
